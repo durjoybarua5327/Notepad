a4565c8be75e6ed7b59d6020f490074fd637024a
Durjoy Barua <142112376+durjoybarua5327@users.noreply.github.com>
2025-03-17 19:05:25 +0600

    commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './index.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className="container bg-red">
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Animi exercitationem deleniti ex adipisci incidunt, aliquam distinctio maiores ab reprehenderit, voluptatem omnis earum sit iure fuga.</p>
+      </div>
+    </>
+  )
+}
+
+export default App
